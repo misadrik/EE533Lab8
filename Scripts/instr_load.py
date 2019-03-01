77c1e3e767496ef5ab1cf9d7fc85fc5dcb519203
MISaD <misad.chien@gmail.com>
2019-03-01 00:06:33 -0800

    write to mm

--- Scripts/instr_load.py
+++ Scripts/instr_load.py
@@ -2,6 +2,9 @@ import os
 
 ADDRESS_Interface = '0x200031c'
 INSTR_Interface = '0x2000318'
+DATA_ADDR_Interface = '0x2000300'
+DATA_LOW_Interface = '0x2000308'
+DATA_HIGH_Interface = '0x2000304'
 
 foutlog = open('./instr_load.log', 'w')
 
@@ -22,9 +25,32 @@ def instr_load():
 def pipeline_disable():
     print('disabling pipeline',file = foutlog)
     pipeline_disable = '0x0000000'
-    # os.system('regwrite ' + ADDRESS_Interface + pipeline_disable)
+    os.system('regwrite ' + ADDRESS_Interface + pipeline_disable)
     print('regwrite ' + ADDRESS_Interface +' '+ pipeline_disable, file=foutlog)
 
+def data_write():
+    data_wr_en = 0x100
+    data_high = ['0x0','0x0','0x0','0x0','0x0']
+    data_low = ['0x2', '0x2000', '4000','0x0','0x0']
+    
+    # number should be less than 256
+    number = data_high.__len__() < data_low.__len__() ?data_high.__len__(): data_low.__len__()
+
+    for num in range(number):
+        os.system('regwrite' + DATA_ADDR_Interface + str(data_wr_en +num))
+        os.system('regwrite' + DATA_HIGH_Interface + data_high[num])
+        os.system('regwrite' + DATA_LOW_Interface + data_low[num])
+        # print('regwrite' + DATA_ADDR_Interface +
+        #                   str(data_wr_en + num))
+        # print('regwrite' + DATA_HIGH_Interface + data_high[num])
+        # print('regwrite' + DATA_LOW_Interface + data_low[num])
+
+def data_write_disable():
+    data_wr_disable = '0x000'
+    os.system('regwrite' + DATA_ADDR_Interface + data_wr_disable)
+    
 if __name__ == "__main__":
     pipeline_disable()
     instr_load()
+    data_write()
+    data_write_disable()
